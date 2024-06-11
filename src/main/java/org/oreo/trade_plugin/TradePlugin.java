737b4f0bc1aef29d9b2b023956c0cc88393f5323
oreotrollturbo <oreotrollturbo@gmail.com>
2024-06-11 12:41:03 +0200

    first

---
+++ src/main/java/org/oreo/trade_plugin/TradePlugin.java
@@ -0,0 +1,17 @@
+package org.oreo.trade_plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TradePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
