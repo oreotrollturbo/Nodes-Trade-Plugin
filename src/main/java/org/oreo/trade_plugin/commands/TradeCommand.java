ebf87542cbd117719e3554ae5b30d107302dbc5e
oreotrollturbo <oreotrollturbo@gmail.com>
2024-06-13 22:16:16 +0200

    first

--- src/main/java/org/oreo/trade_plugin/commands/TradeCommand.java
+++ src/main/java/org/oreo/trade_plugin/commands/TradeCommand.java
@@ -21,23 +21,40 @@ import java.util.*;
 
 public class TradeCommand implements TabExecutor, Listener {
 
-    private static final Map<Player, Player> tradeRequests = new HashMap<>();
-    private static final Map<Player, Inventory> activeTrades = new HashMap<>();
+    private static final Map<Player, Player> tradeRequests = new HashMap<>(); // A list for all trade requests that are active
+    private static final Map<Player, Inventory> activeTrades = new HashMap<>(); // A list for all trade requests happening
 
+    /**
+     * These are used to which player trading corresponds to what inventory
+     * and if they are player 1 or player 2
+     */
     private static final Map<Inventory, Player> inventoryToPlayer1 = new HashMap<>();
     private static final Map<Inventory, Player> inventoryToPlayer2 = new HashMap<>();
 
+    /**
+     * All the slots that correspond to player 1
+     */
     private final List<Integer> player1Slots = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 9, 10, 11, 12, 18, 19,
             20, 21, 27, 28, 29, 30, 36, 37, 38));
 
+    /**
+     * All the slots that correspond to player 1
+     */
     private final List<Integer> player2Slots = new ArrayList<>(Arrays.asList(5, 6, 7, 8, 14, 15, 16, 17, 23, 24,
             25, 26, 32, 33, 34, 35, 42, 43, 44));
 
 
+    /**
+     * The name of the inventory
+     */
     private final String invName = "Trade Offer";
 
-    private final TradePlugin tradePlugin;
+    private final TradePlugin tradePlugin; //Plugin instance
 
+    /**
+     * @param plugin passing in the plugin
+     * Gets the plugin instance and registers events
+     */
     public TradeCommand(TradePlugin plugin) {
         this.tradePlugin = plugin;
         Bukkit.getPluginManager().registerEvents(this, plugin);
@@ -47,7 +64,7 @@ public class TradeCommand implements TabExecutor, Listener {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage("This command can only be used by players.");
-            return true;
+            return true; //Make sure the sender is a player
         }
 
         Player player = (Player) sender;
@@ -72,7 +89,7 @@ public class TradeCommand implements TabExecutor, Listener {
                 tradeRequests.remove(player);
             }
 
-            Inventory tradeInv = createTradeInventory(player, senderPlayer);
+            Inventory tradeInv = createTradeInventory(player, senderPlayer, 5);
 
             synchronized (activeTrades) {
                 activeTrades.put(player, tradeInv);
@@ -129,6 +146,9 @@ public class TradeCommand implements TabExecutor, Listener {
         }
     }
 
+    /**
+     * Adds "accept" to the autoComplete menu
+     */
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         if (args.length == 1){
@@ -146,23 +166,51 @@ public class TradeCommand implements TabExecutor, Listener {
         return null;
     }
 
-    private Inventory createTradeInventory(Player player1, Player player2) {
-        int rows = 5;
+    /**
+     * @param player1 set the first player of the trade
+     * @param player2 set the second player of the trade
+     * @param rows Add how many rows you want the inventory to be
+     * @return inventory
+     *
+     * Creates the trade inventory with the set amount of rows
+     */
+    private Inventory createTradeInventory(Player player1, Player player2, int rows) {
+
         Inventory inv = Bukkit.createInventory(null, 9 * rows, invName);
         initializeItems(inv);
         return inv;
     }
 
+    /**
+     * @param inv pass in the inventory you want the items to be added to
+     * Simple method to initialise items in a new inventory
+     */
     private void initializeItems(Inventory inv) {
         inv.setItem(4, createGuiItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE, " "));
         inv.setItem(13, createGuiItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE, " "));
         inv.setItem(22, createGuiItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE, " "));
         inv.setItem(31, createGuiItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE, " "));
+
+        inv.setItem(44, createGuiItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE, " "));
+        inv.setItem(43, createGuiItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE, " "));
+        inv.setItem(42, createGuiItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE, " "));
+
+        inv.setItem(38, createGuiItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE, " "));
+        inv.setItem(37, createGuiItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE, " "));
+        inv.setItem(36, createGuiItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE, " "));
+
         inv.setItem(40, createGuiItem(Material.BARRIER, "Cancel ready","Cancel the ready state for both players"));
         inv.setItem(39, createGuiItem(Material.RED_WOOL, "Accept trade"));
         inv.setItem(41, createGuiItem(Material.RED_WOOL, "Accept trade"));
     }
 
+    /**
+     * @param material set the material it should be (what item)
+     * @param name How it should be called like any renamed item from an anvil
+     * @param lore This is to set extra text under the name itself it is optional
+     * @return it just returns the item now created
+     * This is a helper method to create items for a gui
+     */
     private ItemStack createGuiItem(final Material material, final String name, final String... lore) {
         final ItemStack item = new ItemStack(material, 1);
         final ItemMeta meta = item.getItemMeta();
@@ -175,10 +223,19 @@ public class TradeCommand implements TabExecutor, Listener {
         return item;
     }
 
+    /**
+     * @param player get the player to know who to open the inventory
+     * @param inv To know what inventory is even getting opened
+     * Opens the already made gui for the player
+     */
     private void openInventory(final Player player, Inventory inv) {
         player.openInventory(inv);
     }
 
+    /**
+     * Checks if it's the trade inventory open , stops shift clicking , stops the COLLECT_TO_CURSOR (double click to gather)
+     * Lets you click on your inventory as long as neither player is "trade ready"
+     */
     @EventHandler
     public void onInventoryClick(final InventoryClickEvent e) {
         if (!e.getView().getTitle().equals(invName)) return;
@@ -191,7 +248,6 @@ public class TradeCommand implements TabExecutor, Listener {
         }
 
         if (e.getAction() == InventoryAction.COLLECT_TO_CURSOR) {
-            e.getWhoClicked().sendMessage("Detected");
 
             e.setResult(Event.Result.DENY);
             e.setCancelled(true);
@@ -272,6 +328,10 @@ public class TradeCommand implements TabExecutor, Listener {
         }
     }
 
+    /**
+     * Checks if either of the players are "ready" by checking if either of the wools are green
+     * if they are then it cancels any click event so you cant drag in/out stuff out of the trade inventory
+     */
     @EventHandler
     private void oneReady(InventoryClickEvent e){
         if (!e.getView().getTitle().equals(invName)) return;
@@ -284,18 +344,42 @@ public class TradeCommand implements TabExecutor, Listener {
         }
     }
 
+    /**
+     * @param rawSlot get the slot clicked
+     * @return
+     * Checks if the items are of the trade gui
+     */
     private boolean isGuiItem(int rawSlot) {
-        return Arrays.asList(4, 13, 22, 31).contains(rawSlot);
+        return Arrays.asList(4, 13, 22, 31,44,43,42,36,37,38).contains(rawSlot);
     }
 
+    /**
+     * @param rawSlot get the slot clicked
+     * @return int
+     * Checks if the clicked slot is for player 1
+     */
     private boolean isPlayer1Slot(int rawSlot) {
         return player1Slots.contains(rawSlot);
     }
 
+    /**
+     * @param rawSlot get the slot clicked
+     * @return boolean
+     * Checks if the slot is for player 2
+     */
     private boolean isPlayer2Slot(int rawSlot) {
         return player2Slots.contains(rawSlot);
     }
 
+    /**
+     * @param rawSlot get the slot clicked
+     * @param player get the player involved
+     * @param tradeInv get the inventory of the trade
+     * @return boolean
+     * Checks if the slot corresponds to the player that clicked
+     * the method does this by checking the player1 list and seeing if the player is in it
+     * if he is not it assumes hes player 2
+     */
     private boolean isOfCorrespondingSlot(int rawSlot, Player player , Inventory tradeInv){
 
         boolean isPlayer1 = inventoryToPlayer1.get(tradeInv).equals(player);
@@ -306,6 +390,14 @@ public class TradeCommand implements TabExecutor, Listener {
     }
 
 
+    /**
+     * @param player Get the player that triggered the method
+     * @param accepted Check if the trade was accepted or denied
+     * @param inv Get the inventory of the trade
+     * This method handles everything when a trade is completed
+     * it closes the inventory for both players sends the appropriate message gives the items
+     * and removes the trade from the list
+     */
     private void handleTradeCompletion(Player player, boolean accepted, Inventory inv) {
         Inventory tradeInv;
         synchronized (activeTrades) {
@@ -358,6 +450,15 @@ public class TradeCommand implements TabExecutor, Listener {
         inv.clear(); // Clear it just in case
     }
 
+    /**
+     * @param player get the player to who caused the event
+     * @param tradePartner get the other player who is in the trade
+     * @param inv get the trade inventory
+     * @param playerSlots gets the first players slots to know who to give the items to
+     * @param partnerSlots gets the second players slots to know who to give the items to
+     * This is a method that's only called by the handleTradeCompletion method
+     * It checks if the trade was completed or cancelled and gives the items accordingly
+     */
     private void processItems(Player player, Player tradePartner, Inventory inv, List<Integer> playerSlots, List<Integer> partnerSlots) {
 
         playerSlots.forEach(slot -> {
@@ -376,6 +477,10 @@ public class TradeCommand implements TabExecutor, Listener {
     }
 
 
+    /**
+     * @param e event
+     * When the trade inventory is closed it calls the handleTradeCompletion method
+     */
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent e) {
         if (!e.getView().getTitle().equals(invName)) return;
@@ -385,6 +490,11 @@ public class TradeCommand implements TabExecutor, Listener {
         handleTradeCompletion(p, false, e.getInventory());
     }
 
+    /**
+     * @param e it's just the event
+     * Makes sure you cant drag items into the other players inventory
+     *          if that happens it
+     */
     @EventHandler
     public void onItemDraged(InventoryDragEvent e){
 
